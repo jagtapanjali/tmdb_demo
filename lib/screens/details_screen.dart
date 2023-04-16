51d6041250d790c2a55bfaf6869ccf4f5beae1f6
Anjali Jagtap <anjali.jagtap@bacancy.com>
2023-04-16 17:12:04 +0530

    Added button of url link and redirected it.

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,6 +6,7 @@ import '../model/movie_details.dart';
 import '../services/movie.dart';
 import '../utils/constants.dart';
 import '../utils/star_calculator.dart';
+import '../utils/url.dart';
 import '../widgets/custom_loading_spin_kit_ring.dart';
 
 class DetailsScreen extends StatefulWidget {
@@ -99,7 +100,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                             height: 4,
                           ),
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
                             child: Wrap(
                               children: [
                                 Text(
@@ -117,7 +118,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                           ),
                           const SizedBox(height: 1),
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
                             child: Row(children: stars!),
                           ),
                         ],
@@ -132,25 +133,57 @@ class _DetailsScreenState extends State<DetailsScreen> {
                               children: [
                                 Padding(
                                   padding: const EdgeInsets.only(
-                                    right: 1,
-                                    left: 1,
+                                    right: 10,
+                                    left: 10,
                                     bottom: 1,
                                   ),
                                   child: Text(kStoryLineTitleText,
                                       style: kSmallTitleTextStyle),
                                 ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 1, left: 1, top: 1, bottom: 4),
-                                    child: Text(
-                                      movieDetails!.overview,
-                                      style: const TextStyle(
-                                          fontSize: 16,
-                                          color: Color(0xFFC9C9C9)),
-                                    ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 10, left: 10, top: 10, bottom: 20),
+                                  child: Text(
+                                    movieDetails!.overview,
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Color(0xFFC9C9C9)),
                                   ),
                                 ),
+                                movieDetails!.homepage != ""
+                                ? Center(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Urls().launchURL(movieDetails!.homepage);
+                                    },
+                                    child: SizedBox(
+                                      width: MediaQuery.of(context).size.width / 1.5,
+                                      child: Card(
+                                        elevation: 29,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(80),
+                                          //set border radius more than 50% of height and width to make circle
+                                        ),
+                                        color: const Color.fromRGBO(65, 65, 73, 1.0),
+                                        child: const Padding(
+                                          padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                          child: Center(
+                                            child: Flexible(
+                                              child: Text(
+                                                'Go to Movie Link >>',
+                                                style: TextStyle(
+                                                  color: Colors.green,
+                                                  fontSize: 19,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                                : SizedBox.shrink(),
                               ],
                             ),
                           ),
---
+++ lib/utils/url.dart
@@ -0,0 +1,12 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class Urls{
+  launchURL(String url) async {
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
